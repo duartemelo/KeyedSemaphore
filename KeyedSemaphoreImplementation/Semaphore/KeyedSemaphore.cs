c5599d9186e427c0948d46103e89fda636e1cb2d
duartemelo <43653646+duartemelo@users.noreply.github.com>
2024-09-07 12:28:12 +0100

    fix(keyedsemaphore): change name of property

--- KeyedSemaphoreImplementation/Semaphore/KeyedSemaphore.cs
+++ KeyedSemaphoreImplementation/Semaphore/KeyedSemaphore.cs
@@ -4,17 +4,17 @@ namespace KeyedSemaphoreImplementation.Semaphore
 {
     public class KeyedSemaphore
     {
-        private static readonly ConcurrentDictionary<string, SemaphoreSlim> semaphoreList = new();
+        private static readonly ConcurrentDictionary<string, SemaphoreSlim> semaphoreDict = new();
 
         public async Task<bool> WaitAsync(string key, int milliseconds)
         {
-            var semaphore = semaphoreList.GetOrAdd(key, _ => new SemaphoreSlim(1, 1)); // if key exists, gets, if not, adds
+            var semaphore = semaphoreDict.GetOrAdd(key, _ => new SemaphoreSlim(1, 1)); // if key exists, gets, if not, adds
             return await semaphore.WaitAsync(milliseconds);
         }
 
         public int Release(string key)
         {
-            if (semaphoreList.TryGetValue(key, out var semaphore))
+            if (semaphoreDict.TryGetValue(key, out var semaphore))
             {
                 return semaphore.Release();
             }
